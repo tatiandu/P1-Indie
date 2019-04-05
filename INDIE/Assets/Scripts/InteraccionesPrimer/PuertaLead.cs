59272e861fd19361cc07e9de591c7e0488d8cc83
Daniel <dangon21@ucm.es>
2019-04-05 14:29:19 +0200

    A

---
+++ INDIE/Assets/Scripts/InteraccionesPrimer/PuertaLead.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuertaLead : MonoBehaviour
+{
+
+    public GameObject Abierta, Cerrada, Lead;
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        if (collision.gameObject == Lead)
+        {
+            Abierta.SetActive(true);
+            Cerrada.SetActive(false);
+        }
+    }
+}
