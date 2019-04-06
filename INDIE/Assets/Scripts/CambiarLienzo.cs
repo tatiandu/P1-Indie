97e273f1f94cd5844946ad0496ee02832eb093b9
davidczepiel <48888283+davidczepiel@users.noreply.github.com>
2019-04-06 17:38:52 +0200

    Añadidos varios coleccionables e interactuables al nivel 3

---
+++ INDIE/Assets/Scripts/CambiarLienzo.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CambiarLienzo : MonoBehaviour {
+
+    public GameObject lienzoQueCambio;
+    public GameObject lienzoNuevo;
+    public void OnEnable()
+    {
+        lienzoQueCambio.SetActive(false);
+        lienzoNuevo.SetActive(true);
+
+    }
+}
