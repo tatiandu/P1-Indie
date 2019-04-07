877610e30e330bfbec343ef728e90e4b996242c4
davidczepiel <48888283+davidczepiel@users.noreply.github.com>
2019-04-07 20:07:12 +0200

    Añadidas animaciones de la camara (nivel3)

---
+++ INDIE/Assets/ReproducirAnimación.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReproducirAnimación : MonoBehaviour {
+
+    public GameObject animacion;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (GameManager.instance.CaosActual() == 100)
+        {
+            animacion.SetActive(true);
+        }
+	}
+}
