713dba0510e0346472f8f8263ada1be463d779a9
PabloVillapunMartin <48910465+PabloVillapunMartin@users.noreply.github.com>
2019-04-08 18:23:55 +0200

    arreglos NV2 y mejora script de detección

--- INDIE/Assets/Scripts/ConoDeVision.cs
+++ INDIE/Assets/Scripts/ConoDeVision.cs
@@ -25,17 +25,30 @@ public class ConoDeVision : MonoBehaviour {
             ray = Physics2D.Raycast(transform.position, playerGO.transform.position - transform.position, Vector2.Distance(playerGO.transform.position, transform.position));  //Raycast
             Debug.DrawRay(transform.position, playerGO.transform.position - transform.position, Color.red);    //Debug del raycast
 
-            if (ray.collider.tag == "Player" && tipoEnemigo.TipoDeEnemigo() == Disfraz.programador && (GameManager.instance.DisfrazJugador() == Disfraz.programador || GameManager.instance.DisfrazJugador() == Disfraz.ninguno)
-            || tipoEnemigo.TipoDeEnemigo() == Disfraz.artista && (GameManager.instance.DisfrazJugador() == Disfraz.artista || GameManager.instance.DisfrazJugador() == Disfraz.diseñador || GameManager.instance.DisfrazJugador() == Disfraz.ninguno)
-            || tipoEnemigo.TipoDeEnemigo() == Disfraz.diseñador && (GameManager.instance.DisfrazJugador() == Disfraz.diseñador || GameManager.instance.DisfrazJugador() == Disfraz.programador || GameManager.instance.DisfrazJugador() == Disfraz.artista || GameManager.instance.DisfrazJugador() == Disfraz.ninguno)
-            || tipoEnemigo.TipoDeEnemigo() == Disfraz.personal || tipoEnemigo.TipoDeEnemigo() == Disfraz.lead)
+            if (ray.collider.tag == "Player" && CompruebaDeteccion())
             {
                 Debug.Log("Player detected");
                 movEnemigo.CambiaEstado(Estados.persecucion);
             }
         }
     }
-    
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            playerGO = collision.gameObject;
+            ray = Physics2D.Raycast(transform.position, playerGO.transform.position - transform.position, Vector2.Distance(playerGO.transform.position, transform.position));  //Raycast
+            Debug.DrawRay(transform.position, playerGO.transform.position - transform.position, Color.red);    //Debug del raycast
+
+            if (ray.collider.tag == "Player" && CompruebaDeteccion())
+            {
+                Debug.Log("Player detected");
+                movEnemigo.CambiaEstado(Estados.persecucion);
+            }
+        }
+    }
+
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.tag == "Player")
@@ -45,15 +58,11 @@ public class ConoDeVision : MonoBehaviour {
         }
     }
 
-    //// Activa el sprite
-    //public void ActivarRender()
-    //{
-    //    render.enabled = true;
-    //}
-
-    //// Desactiva el sprite
-    //public void DesactivarRender()
-    //{
-    //    render.enabled = false;
-    //}
+    private bool CompruebaDeteccion()
+    {
+        return tipoEnemigo.TipoDeEnemigo() == Disfraz.programador && (GameManager.instance.DisfrazJugador() == Disfraz.programador || GameManager.instance.DisfrazJugador() == Disfraz.ninguno)
+            || tipoEnemigo.TipoDeEnemigo() == Disfraz.artista && (GameManager.instance.DisfrazJugador() == Disfraz.artista || GameManager.instance.DisfrazJugador() == Disfraz.diseñador || GameManager.instance.DisfrazJugador() == Disfraz.ninguno)
+            || tipoEnemigo.TipoDeEnemigo() == Disfraz.diseñador && (GameManager.instance.DisfrazJugador() == Disfraz.diseñador || GameManager.instance.DisfrazJugador() == Disfraz.programador || GameManager.instance.DisfrazJugador() == Disfraz.artista || GameManager.instance.DisfrazJugador() == Disfraz.ninguno)
+            || tipoEnemigo.TipoDeEnemigo() == Disfraz.personal || tipoEnemigo.TipoDeEnemigo() == Disfraz.lead;
+    }
 }
