b6baa771b96575fbbab5ecac07fa64242d51170d
davidczepiel <48888283+davidczepiel@users.noreply.github.com>
2019-04-19 17:49:41 +0200

    El selector de niveles solo muestra los niveles que hayas completado según el archivo partida.txt

--- INDIE/Assets/Scripts/Menuprincipal.cs
+++ INDIE/Assets/Scripts/Menuprincipal.cs
@@ -5,18 +5,48 @@ using UnityEngine.Audio;
 using UnityEngine.EventSystems;
 using System.IO;
 
-public class Menuprincipal : MonoBehaviour {
+public class Menuprincipal : MonoBehaviour
+{
     public GameObject botonesPrincipales, niveles, ajustes, continuar, fondo, fondoNiveles;
     public AudioMixer audioMixer;
+    public GameObject nivel1, nivel2, nivel3;
 
-    void Start () {
-        if (!File.Exists("partida.txt"))
+    void Start()
+    {
+        if (!File.Exists("partida.txt"))     //Si no existe continuar no debe salir y activo el nivel 1
         {
             continuar.SetActive(false);
+            nivel1.SetActive(true);
+            StreamWriter archivo = new StreamWriter("partida.txt");  //Creo el archivo y pongo el nivel 1 pero sin activar el "continuar"
+            archivo.WriteLine("1");
+            archivo.Close();
+        }
+        else
+        {            
+            StreamReader cargar = new StreamReader("partida.txt");  // si existe leo la primera linea para ver cuántos niveles tengo que mostrar
+            //Leemos la escena que debemos cargar
+            int numeroNiveles = int.Parse(cargar.ReadLine());
+            cargar.Close();
+            switch (numeroNiveles)
+            {
+                case 1:
+                    nivel1.SetActive(true);
+
+                    break;
+                case 2:
+                    nivel1.SetActive(true);
+                    nivel2.SetActive(true);
+                    break;
+                case 3:
+                    nivel1.SetActive(true);
+                    nivel2.SetActive(true);
+                    nivel3.SetActive(true);
+                    break;
+            }
         }
     }
-	
-	
+
+
 
 
     public void CargarNivel(int escena)
@@ -35,16 +65,16 @@ public class Menuprincipal : MonoBehaviour {
                 GameManager.instance.CambioDisfrazJugador(Disfraz.ninguno);
                 break;
         }
-        
+
     }
     public void SelecciónNiveles()
     {
+
         botonesPrincipales.SetActive(false);
         niveles.SetActive(true);
-
         fondo.SetActive(false);
         fondoNiveles.SetActive(true);
-        
+
     }
     public void VolverMenu()
     {
@@ -75,7 +105,7 @@ public class Menuprincipal : MonoBehaviour {
     {
         Screen.fullScreen = pantallaCompleta;
     }
-     public void Continuar()
+    public void Continuar()
     {
         GameManager.instance.CargarPartida();
     }
