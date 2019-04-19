d711e63f97327af129175937b3413dd5bd183e66
davidczepiel <48888283+davidczepiel@users.noreply.github.com>
2019-04-19 15:12:04 +0200

    Invulnerabilidad animaciones nivel 3 implementada

--- INDIE/Assets/ReproducirAnimación.cs
+++ INDIE/Assets/ReproducirAnimación.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ReproducirAnimación : MonoBehaviour {
+public class ReproducirAnimación : MonoBehaviour
+{
     public GameObject animacionInicial;
     public GameObject animacionCaos;
     public GameObject player;
@@ -12,27 +13,41 @@ public class ReproducirAnimación : MonoBehaviour {
     bool segundaAnimacion;
     float horaSegundaAnimacion;
     public float tiempoSegundaAnimacion;
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         animacionInicial.gameObject.SetActive(true);
-        primeraAnimacion = false;
-        primeraAnimacion = false;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (GameManager.instance.CaosActual() == 100)
+        primeraAnimacion = true;
+        horaPrimeraAnimacion = Time.time;
+        segundaAnimacion = false;
+        player.GetComponent<Perder>().enabled = false;
+        player.GetComponent<movimiento>().enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameManager.instance.CaosActual() == 100 && !segundaAnimacion)
         {
             segundaAnimacion = true;
             horaSegundaAnimacion = Time.time;
             animacionCaos.gameObject.SetActive(true);
-            
-            
+            player.GetComponent<Perder>().enabled = false;
+            player.GetComponent<movimiento>().enabled = false;
+
+
+
         }
-        if(segundaAnimacion && Time.time < horaSegundaAnimacion + tiempoSegundaAnimacion)
+        if (primeraAnimacion && !segundaAnimacion && Time.time > horaPrimeraAnimacion + tiempoPrimeraAnimacion)
         {
-            Perder perder = player.GetComponent<Perder>();
-            perder.enabled = false;
+            player.GetComponent<Perder>().enabled = true;
+            player.GetComponent<movimiento>().enabled = true;
         }
-	}
+        if (segundaAnimacion && Time.time > horaSegundaAnimacion + tiempoSegundaAnimacion/* && Input.GetKey(KeyCode.W)*/)
+        {
+            player.GetComponent<Perder>().enabled = true;
+            player.GetComponent<movimiento>().enabled = true;
+        }
+
+    }
 }
