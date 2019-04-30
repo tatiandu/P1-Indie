ae034f3bb4150990e98e64c956c70046c164abfb
davidczepiel <48888283+davidczepiel@users.noreply.github.com>
2019-04-30 23:09:55 +0200

    Añadido puntero que te indica donde está el ascensor cuando has conseguido la tarjeta

---
+++ INDIE/Assets/Scripts/TargetIndicator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetIndicator : MonoBehaviour {
+
+    public Transform target;
+    public GameObject flecha;
+	
+	
+	// Update is called once per frame
+	void Update () {
+        var dir = target.position - transform.position;
+        var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        if (GameManager.instance.CaosActual() >= 100)
+        {
+            flecha.SetActive(true);
+        }
+	}
+}
