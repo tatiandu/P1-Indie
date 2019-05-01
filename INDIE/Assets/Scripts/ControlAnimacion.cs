91b0eb9af10b41a7b1450d3a89078ab823bfaae0
PabloVillapunMartin <48910465+PabloVillapunMartin@users.noreply.github.com>
2019-05-01 19:40:55 +0200

    ControlAnimación eliminado y cambio en el mensaje inicial nv1

---
+++ INDIE/Assets/Scripts/ControlAnimacion.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlAnimacion : MonoBehaviour {
+
+    public int tiempoDeEspera;
+    public GameObject go;
+    private float time;
+
+    private void Start()
+    {
+        //go.SetActive(false);
+        time = Time.time + tiempoDeEspera;
+    }
+
+    private void Update()
+    {
+        if (Time.time <= time)
+        {
+            go.SetActive(true);
+            Destroy(this);
+        }
+    }
+}
