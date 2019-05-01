97afc63c579491ee2202edb45681e23f146bf182
davidczepiel <48888283+davidczepiel@users.noreply.github.com>
2019-05-01 10:39:40 +0200

    Solucionado que los enemigos no hagan la animación de andar al moverse

--- INDIE/Assets/Scripts/MovimientoEnemigo.cs
+++ INDIE/Assets/Scripts/MovimientoEnemigo.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MovimientoEnemigo : MonoBehaviour {
+public class MovimientoEnemigo : MonoBehaviour
+{
 
     public int velocidadEstandar, velocidadPersecucion;
     public Transform[] puntosPatrulla;
@@ -18,7 +19,8 @@ public class MovimientoEnemigo : MonoBehaviour {
     Animator anim;
     public bool fueraZona;
 
-	void Start () {
+    void Start()
+    {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
         estado = Estados.volviendo;
@@ -27,9 +29,10 @@ public class MovimientoEnemigo : MonoBehaviour {
         i = 0;
         Physics2D.queriesStartInColliders = false;
     }
-	
-	void Update () {
-		
+
+    void Update()
+    {
+
         if (puntosPatrulla.Length > 0)
         {
             switch (estado)     //Maquina de estados
@@ -51,134 +54,141 @@ public class MovimientoEnemigo : MonoBehaviour {
                     break;
             }
         }
-	}
-
-    void EstadoPatrulla()
-    {
-        if (puntosPatrulla.Length > 1)      //Si el NPC va a seguir un patron
+        if (Mathf.Abs(rb.velocity.x) > 0.01 || Mathf.Abs(rb.velocity.y) > 0.01)
         {
-            if (puntosPatrulla[i % puntosPatrulla.Length] != null &&
-                Vector2.Distance(puntosPatrulla[i % puntosPatrulla.Length].position, transform.position) > 0.4f)     //Movimiento hasta llegar al siguiente punto
-            {
-                if (esGuardia && i >= puntosPatrulla.Length && !recorridoGuardiaRealizado)
-                {
-                    InvierteCamino();
-                    i = 0;
-                    recorridoGuardiaRealizado = true;
-                    anim.SetBool("IsMoving", false);
-
-                }
-                if (esGuardia && i >= puntosPatrulla.Length && recorridoGuardiaRealizado)
-                {
-                    recorridoGuardiaRealizado = false;
-                    i = 0;
-                    gameObject.SetActive(false);
-                    anim.SetBool("IsMoving", true);
-
-                }
-                if(esGuardia && !this.gameObject.activeSelf)
-                {
-                    recorridoGuardiaRealizado = false;
-                }
-                direccion = new Vector2(puntosPatrulla[i % puntosPatrulla.Length].position.x - transform.position.x, puntosPatrulla[i % puntosPatrulla.Length].position.y - transform.position.y).normalized;
-                transform.right = direccion;
-                rb.velocity = transform.right * velocidadEstandar;
-            }
-            else i++;
+            anim.SetBool("IsMoving", true);
         }
         else
         {
             anim.SetBool("IsMoving", false);
-        transform.right = direccionInicial;     //Se queda mirando a la posicion inicial
-            rb.velocity = Vector2.zero;
         }
-
-        rb.angularVelocity = 0;
     }
-
-    void EstadoPersecucion()
-    {
-        ray = Physics2D.Raycast(transform.position, playerGO.transform.position - transform.position, Vector2.Distance(playerGO.transform.position, transform.position));  //Raycast
-        Debug.DrawRay(transform.position, playerGO.transform.position - transform.position, Color.red);    //Debug del raycast
-
-        if (ray.transform.tag == "Player")      //Si el tag es player le persigue
+        void EstadoPatrulla()
         {
-            if (exclamationSprite == null) exclamationSprite = Instantiate<GameObject>(exclamationPrefab);
-            direccion = new Vector2(playerGO.transform.position.x - transform.position.x, playerGO.transform.position.y - transform.position.y).normalized;
-            transform.right = direccion;
-            rb.velocity = transform.right * velocidadPersecucion;
+            if (puntosPatrulla.Length > 1)      //Si el NPC va a seguir un patron
+            {
+                if (puntosPatrulla[i % puntosPatrulla.Length] != null &&
+                    Vector2.Distance(puntosPatrulla[i % puntosPatrulla.Length].position, transform.position) > 0.4f)     //Movimiento hasta llegar al siguiente punto
+                {
+                    if (esGuardia && i >= puntosPatrulla.Length && !recorridoGuardiaRealizado)
+                    {
+                        InvierteCamino();
+                        i = 0;
+                        recorridoGuardiaRealizado = true;
+                        anim.SetBool("IsMoving", false);
+
+                    }
+                    if (esGuardia && i >= puntosPatrulla.Length && recorridoGuardiaRealizado)
+                    {
+                        recorridoGuardiaRealizado = false;
+                        i = 0;
+                        gameObject.SetActive(false);
+                        anim.SetBool("IsMoving", true);
+
+                    }
+                    if (esGuardia && !this.gameObject.activeSelf)
+                    {
+                        recorridoGuardiaRealizado = false;
+                    }
+                    direccion = new Vector2(puntosPatrulla[i % puntosPatrulla.Length].position.x - transform.position.x, puntosPatrulla[i % puntosPatrulla.Length].position.y - transform.position.y).normalized;
+                    transform.right = direccion;
+                    rb.velocity = transform.right * velocidadEstandar;
+                }
+                else i++;
+            }
+            else
+            {
+                anim.SetBool("IsMoving", false);
+                transform.right = direccionInicial;     //Se queda mirando a la posicion inicial
+                rb.velocity = Vector2.zero;
+            }
+
+            rb.angularVelocity = 0;
         }
-        else CambiaEstado(Estados.volviendo);
 
-        if (exclamationSprite != null)  exclamationSprite.transform.position = transform.position + new Vector3(0f, 2f, 0f);
-
-        rb.angularVelocity = 0;
-    }
-
-    void EstadoVolviendo()
-    {
-        ray = Physics2D.Raycast(transform.position, puntosPatrulla[i % puntosPatrulla.Length].transform.position - transform.position, Vector2.Distance(puntosPatrulla[i % puntosPatrulla.Length].transform.position, transform.position)); //Raycast
-        Debug.DrawRay(transform.position, puntosPatrulla[i % puntosPatrulla.Length].transform.position - transform.position, Color.blue);       //Debug del raycast
-
-        if (ray.transform != null && ray.transform.tag != "Player") i++;         //Cambiar punto de destino si se interpone un obstaculo en medio
-
-        if (Vector2.Distance(puntosPatrulla[i % puntosPatrulla.Length].position, transform.position) > 0.4f)     //Movimiento del NPC hasta que llega al punto
+        void EstadoPersecucion()
         {
-            direccion = new Vector2(puntosPatrulla[i % puntosPatrulla.Length].transform.position.x - transform.position.x, puntosPatrulla[i % puntosPatrulla.Length].transform.position.y - transform.position.y).normalized;
-            transform.right = direccion;
-            rb.velocity = transform.right * velocidadEstandar;
+            ray = Physics2D.Raycast(transform.position, playerGO.transform.position - transform.position, Vector2.Distance(playerGO.transform.position, transform.position));  //Raycast
+            Debug.DrawRay(transform.position, playerGO.transform.position - transform.position, Color.red);    //Debug del raycast
+
+            if (ray.transform.tag == "Player")      //Si el tag es player le persigue
+            {
+                if (exclamationSprite == null) exclamationSprite = Instantiate<GameObject>(exclamationPrefab);
+                direccion = new Vector2(playerGO.transform.position.x - transform.position.x, playerGO.transform.position.y - transform.position.y).normalized;
+                transform.right = direccion;
+                rb.velocity = transform.right * velocidadPersecucion;
+            }
+            else CambiaEstado(Estados.volviendo);
+
+            if (exclamationSprite != null) exclamationSprite.transform.position = transform.position + new Vector3(0f, 2f, 0f);
+
+            rb.angularVelocity = 0;
         }
-        else CambiaEstado(Estados.patrulla);
 
-        rb.angularVelocity = 0;
-    }
-
-    public void CambiaEstado(Estados nuevoEstado)
-    {
-        estado = nuevoEstado;
-    }
-
-    public void PlayerPerdido()
-    {
-        CambiaEstado(Estados.volviendo);
-        if (exclamationSprite != null) Destroy(exclamationSprite);
-    }
-
-    public void AbandonoZona()
-    {
-        CambiaEstado(Estados.volviendo);
-        if (exclamationSprite != null) Destroy(exclamationSprite);
-        Debug.Log("zona");
-    }
-
-    public void CambioPatron(Transform[] puntosnuevos)
-    {
-        i = 0;
-        puntosPatrulla = puntosnuevos;
-    }
-
-    public bool Hellegado()
-    {
-        if (puntosPatrulla[puntosPatrulla.Length - 1] !=null && Vector2.Distance(transform.position, new Vector2(puntosPatrulla[puntosPatrulla.Length - 1].position.x, puntosPatrulla[puntosPatrulla.Length - 1].position.y)) < 1f)
+        void EstadoVolviendo()
         {
-            return true;
-        }
-        else return false;
-    }
+            ray = Physics2D.Raycast(transform.position, puntosPatrulla[i % puntosPatrulla.Length].transform.position - transform.position, Vector2.Distance(puntosPatrulla[i % puntosPatrulla.Length].transform.position, transform.position)); //Raycast
+            Debug.DrawRay(transform.position, puntosPatrulla[i % puntosPatrulla.Length].transform.position - transform.position, Color.blue);       //Debug del raycast
 
-    public Estados Estado()
-    {
-        return estado;
-    }
-    
-    void InvierteCamino()
-    {
-        Transform aux;
-        for (int i = 0; i < puntosPatrulla.Length / 2; i++)
+            if (ray.transform != null && ray.transform.tag != "Player") i++;         //Cambiar punto de destino si se interpone un obstaculo en medio
+
+            if (Vector2.Distance(puntosPatrulla[i % puntosPatrulla.Length].position, transform.position) > 0.4f)     //Movimiento del NPC hasta que llega al punto
+            {
+                direccion = new Vector2(puntosPatrulla[i % puntosPatrulla.Length].transform.position.x - transform.position.x, puntosPatrulla[i % puntosPatrulla.Length].transform.position.y - transform.position.y).normalized;
+                transform.right = direccion;
+                rb.velocity = transform.right * velocidadEstandar;
+            }
+            else CambiaEstado(Estados.patrulla);
+
+            rb.angularVelocity = 0;
+        }
+
+        public void CambiaEstado(Estados nuevoEstado)
         {
-            aux = puntosPatrulla[i];
-            puntosPatrulla[i] = puntosPatrulla[puntosPatrulla.Length - i - 1];
-            puntosPatrulla[puntosPatrulla.Length - i - 1] = aux;
+            estado = nuevoEstado;
+        }
+
+        public void PlayerPerdido()
+        {
+            CambiaEstado(Estados.volviendo);
+            if (exclamationSprite != null) Destroy(exclamationSprite);
+        }
+
+        public void AbandonoZona()
+        {
+            CambiaEstado(Estados.volviendo);
+            if (exclamationSprite != null) Destroy(exclamationSprite);
+            Debug.Log("zona");
+        }
+
+        public void CambioPatron(Transform[] puntosnuevos)
+        {
+            i = 0;
+            puntosPatrulla = puntosnuevos;
+        }
+
+        public bool Hellegado()
+        {
+            if (puntosPatrulla[puntosPatrulla.Length - 1] != null && Vector2.Distance(transform.position, new Vector2(puntosPatrulla[puntosPatrulla.Length - 1].position.x, puntosPatrulla[puntosPatrulla.Length - 1].position.y)) < 1f)
+            {
+                return true;
+            }
+            else return false;
+        }
+
+        public Estados Estado()
+        {
+            return estado;
+        }
+
+        void InvierteCamino()
+        {
+            Transform aux;
+            for (int i = 0; i < puntosPatrulla.Length / 2; i++)
+            {
+                aux = puntosPatrulla[i];
+                puntosPatrulla[i] = puntosPatrulla[puntosPatrulla.Length - i - 1];
+                puntosPatrulla[puntosPatrulla.Length - i - 1] = aux;
+            }
         }
     }
-}
