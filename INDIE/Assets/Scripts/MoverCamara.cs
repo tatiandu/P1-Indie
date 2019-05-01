29fb52b3185e74da1289003a591c7a974ae267c3
Antonio Jesús Guerra Garduño <antgue01@ucm.es>
2019-05-01 20:19:46 +0200

    Se puede mover la cámara en el eje positivo de las x o de las y

---
+++ INDIE/Assets/Scripts/MoverCamara.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoverCamara : MonoBehaviour
+{
+    public CinemachineCameraOffset offset;
+    public float offsetX, offsetY, maxOffset;
+    float valor;
+
+    private void Start()
+    {
+        valor = 0;
+    }
+    void Update()
+    {
+        if (Input.GetButton("Mouse X") && Mathf.Abs(offset.m_Offset.x) < Mathf.Abs(maxOffset))
+        {
+            Debug.Log("x");
+            offset.m_Offset.x += offsetX;
+        }
+        else if (Input.GetButton("Mouse X") && Mathf.Abs(offset.m_Offset.x) < Mathf.Abs(maxOffset))
+        {
+            Debug.Log("-x");
+            offset.m_Offset.x -= offsetX;
+        }
+        else if (Input.GetButton("Mouse Y") && Mathf.Abs(offset.m_Offset.y) < Mathf.Abs(maxOffset))
+        {
+            Debug.Log("y");
+            offset.m_Offset.y += offsetY;
+        }
+        else if (Input.GetButton("Mouse Y")  && Mathf.Abs(offset.m_Offset.y) < Mathf.Abs(maxOffset))
+        {
+            Debug.Log("-y");
+            offset.m_Offset.y -= offsetY;
+        }
+        else
+        {
+            offset.m_Offset = Vector2.zero;
+        }
+
+
+    }
+}
