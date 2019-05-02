9ac84ad4a4058a6330af2c264e95e08f04e923f2
davidczepiel <48888283+davidczepiel@users.noreply.github.com>
2019-05-02 11:47:23 +0200

    Ahora puedes mover un poco la cámara con el joystick derecho

--- INDIE/Assets/Scripts/MoverCamara.cs
+++ INDIE/Assets/Scripts/MoverCamara.cs
@@ -7,6 +7,8 @@ public class MoverCamara : MonoBehaviour
     public CinemachineCameraOffset offset;
     public float offsetX, offsetY, maxOffset;
     float valor;
+    public float EjeX;
+    public float EjeY;
 
     private void Start()
     {
@@ -14,25 +16,50 @@ public class MoverCamara : MonoBehaviour
     }
     void Update()
     {
-        if (Input.GetButton("Mouse X") && Mathf.Abs(offset.m_Offset.x) < Mathf.Abs(maxOffset))
+        EjeX = Input.GetAxis("Mouse X");
+        EjeY = Input.GetAxis("Mouse Y");
+        //if (Input.GetAxis("Mouse X")>0 && Mathf.Abs(offset.m_Offset.x) < Mathf.Abs(maxOffset))
+        //{
+        //    Debug.Log("x");
+        //    offset.m_Offset.x += offsetX;
+        //}
+        //else if (Input.GetAxis("Mouse X")<0 && Mathf.Abs(offset.m_Offset.x) < Mathf.Abs(maxOffset))
+        //{
+        //    Debug.Log("-x");
+        //    offset.m_Offset.x -= offsetX;
+        //}
+        //else if (Input.GetAxis("Mouse Y")>0 && Mathf.Abs(offset.m_Offset.y) < Mathf.Abs(maxOffset))
+        //{
+        //    Debug.Log("y");
+        //    offset.m_Offset.y -= offsetY;
+        //}
+        //else if (Input.GetAxis("Mouse Y")<0  && Mathf.Abs(offset.m_Offset.y) < Mathf.Abs(maxOffset))
+        //{
+        //    Debug.Log("-y");
+        //    offset.m_Offset.y += offsetY;
+        //}
+        //else
+        //{
+        //    offset.m_Offset = Vector2.zero;
+        //}
+        if((Input.GetAxis("Mouse X") > 0 || Input.GetAxis("Mouse X") < 0) || (Input.GetAxis("Mouse Y") < 0 || Input.GetAxis("Mouse Y") > 0))
         {
-            Debug.Log("x");
-            offset.m_Offset.x += offsetX;
-        }
-        else if (Input.GetButton("Mouse X") && Mathf.Abs(offset.m_Offset.x) < Mathf.Abs(maxOffset))
-        {
-            Debug.Log("-x");
-            offset.m_Offset.x -= offsetX;
-        }
-        else if (Input.GetButton("Mouse Y") && Mathf.Abs(offset.m_Offset.y) < Mathf.Abs(maxOffset))
-        {
-            Debug.Log("y");
-            offset.m_Offset.y += offsetY;
-        }
-        else if (Input.GetButton("Mouse Y")  && Mathf.Abs(offset.m_Offset.y) < Mathf.Abs(maxOffset))
-        {
-            Debug.Log("-y");
-            offset.m_Offset.y -= offsetY;
+            if(Input.GetAxis("Mouse X") > 0 && Mathf.Abs(offset.m_Offset.x)<maxOffset)
+            {
+                offset.m_Offset.x += offsetX;
+            }
+            else if (Input.GetAxis("Mouse X") < 0 && Mathf.Abs(offset.m_Offset.x) < maxOffset)
+            {
+                offset.m_Offset.x -= offsetX;
+            }
+            if (Input.GetAxis("Mouse Y") < 0 && Mathf.Abs(offset.m_Offset.y) < maxOffset)
+            {
+                offset.m_Offset.y += offsetY;
+            }
+            else if (Input.GetAxis("Mouse Y") > 0 && Mathf.Abs(offset.m_Offset.y) < maxOffset)
+            {
+                offset.m_Offset.y -= offsetY;
+            }
         }
         else
         {
